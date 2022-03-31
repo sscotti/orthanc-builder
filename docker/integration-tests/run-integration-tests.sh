416cb0ec9df7c6a6f722f2c24074b923669469c2
Alain Mazy <alain@mazy.be>
2022-03-31 16:18:51 +0200

    fix integ test version

--- docker/integration-tests/run-integration-tests.sh
+++ docker/integration-tests/run-integration-tests.sh
@@ -6,7 +6,7 @@ branch_tag_name=${1:-unknown}
 docker build --build-arg IMAGE_TAG=$branch_tag_name -f orthanc-under-tests/Dockerfile -t orthanc-under-tests orthanc-under-tests
 
 # CHANGE_ORTHANC_TESTS_VERSION
-orthanc_tests_revision=ee398adfeb8e
+orthanc_tests_revision=4e944cedf1a9
 docker build --build-arg ORTHANC_TESTS_REVISION=$orthanc_tests_revision -f orthanc-tests/Dockerfile --target orthanc-tests -t orthanc-tests orthanc-tests
 docker build --build-arg ORTHANC_TESTS_REVISION=$orthanc_tests_revision -f orthanc-tests/Dockerfile --target orthanc-tests-dicomweb -t orthanc-tests-dicomweb orthanc-tests
 docker build --build-arg ORTHANC_TESTS_REVISION=$orthanc_tests_revision -f orthanc-tests/Dockerfile --target orthanc-tests-worklists -t orthanc-tests-worklists orthanc-tests
